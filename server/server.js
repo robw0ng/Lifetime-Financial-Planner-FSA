19403c670e81e9e763313e034b68ed147299ac08
Robert Wong <robwong15@gmail.com>
2025-04-01 19:58:32 -0400

    Fix: use dynamic port for Heroku

--- server/server.js
+++ server/server.js
@@ -1,70 +1,53 @@
-// const cors = require("cors");
-// require("dotenv").config();
-// const express = require("express");
-// const session = require("express-session");
-// const passport = require("passport");
-// const path = require("path");
-// const { Sequelize } = require("sequelize");
-
-// // TODO: any extra db config or init script
-// // middleware
-// const app = express();
-// app.use(
-// 	cors({
-// 		origin: "http://localhost:5173",
-// 		credentials: true,
-// 	})
-// );
-// app.use(
-// 	session({
-// 		secret: process.env.SESSION_SECRET,
-// 		resave: false,
-// 		saveUninitialized: false,
-// 		cookie: {
-// 			httpOnly: true,
-// 			secure: false,
-// 			maxAge: 10 * 60 * 1000,
-// 			sameSite: "lax",
-// 		},
-// 	})
-// );
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// //TODO: routing goes here
-// const scenarioController = require("./controllers/scenario");
-// const authController = require("./controllers/auth");
-// const eventController = require("./controllers/eventseries");
-// const investmentController = require("./controllers/investment");
-// const investmentTypeController = require("./controllers/investmenttype");
-// app.use("/scenarios", scenarioController);
-// app.use("/auth", authController);
-// app.use("/events", eventController);
-// app.use("/investments", investmentController);
-// app.use("/investmenttypes", investmentTypeController);
-
-// // listening
-// app.listen(8000, () => {
-// 	console.log("Server started on port 8000");
-// });
-
+const cors = require("cors");
 require("dotenv").config();
 const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const path = require("path");
+const { Sequelize } = require("sequelize");
+const PORT = process.env.PORT || 8000;
+
+
+// TODO: any extra db config or init script
+// middleware
 const app = express();
-const db = require("./models");
-
-const PORT = process.env.PORT || 3000;
-
+app.use(
+	cors({
+		origin: "http://localhost:5173",
+		credentials: true,
+	})
+);
+app.use(
+	session({
+		secret: process.env.SESSION_SECRET,
+		resave: false,
+		saveUninitialized: false,
+		cookie: {
+			httpOnly: true,
+			secure: false,
+			maxAge: 10 * 60 * 1000,
+			sameSite: "lax",
+		},
+	})
+);
 app.use(express.json());
-app.get("/", (req, res) => {
-	res.send("Hello from Heroku!");
-});
+app.use(express.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-// Sync models and start server
-db.sequelize.sync().then(() => {
-	app.listen(PORT, () => {
-		console.log(`Server running on port ${PORT}`);
-	});
+//TODO: routing goes here
+const scenarioController = require("./controllers/scenario");
+const authController = require("./controllers/auth");
+const eventController = require("./controllers/eventseries");
+const investmentController = require("./controllers/investment");
+const investmentTypeController = require("./controllers/investmenttype");
+app.use("/scenarios", scenarioController);
+app.use("/auth", authController);
+app.use("/events", eventController);
+app.use("/investments", investmentController);
+app.use("/investmenttypes", investmentTypeController);
+
+// listening
+app.listen(PORT, () => {
+	console.log(`Server started on port ${PORT}`);
 });
