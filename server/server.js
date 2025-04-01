d84b90eba672a255f1ad7db39d3cf692ea813bae
Robert Wong <robwong15@gmail.com>
2025-04-01 19:46:04 -0400

    configure monorepo with server/

--- server/server.js
+++ server/server.js
@@ -1,51 +1,70 @@
-const cors = require("cors");
+// const cors = require("cors");
+// require("dotenv").config();
+// const express = require("express");
+// const session = require("express-session");
+// const passport = require("passport");
+// const path = require("path");
+// const { Sequelize } = require("sequelize");
+
+// // TODO: any extra db config or init script
+// // middleware
+// const app = express();
+// app.use(
+// 	cors({
+// 		origin: "http://localhost:5173",
+// 		credentials: true,
+// 	})
+// );
+// app.use(
+// 	session({
+// 		secret: process.env.SESSION_SECRET,
+// 		resave: false,
+// 		saveUninitialized: false,
+// 		cookie: {
+// 			httpOnly: true,
+// 			secure: false,
+// 			maxAge: 10 * 60 * 1000,
+// 			sameSite: "lax",
+// 		},
+// 	})
+// );
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// //TODO: routing goes here
+// const scenarioController = require("./controllers/scenario");
+// const authController = require("./controllers/auth");
+// const eventController = require("./controllers/eventseries");
+// const investmentController = require("./controllers/investment");
+// const investmentTypeController = require("./controllers/investmenttype");
+// app.use("/scenarios", scenarioController);
+// app.use("/auth", authController);
+// app.use("/events", eventController);
+// app.use("/investments", investmentController);
+// app.use("/investmenttypes", investmentTypeController);
+
+// // listening
+// app.listen(8000, () => {
+// 	console.log("Server started on port 8000");
+// });
+
 require("dotenv").config();
 const express = require("express");
-const session = require("express-session");
-const passport = require("passport");
-const path = require("path");
-const { Sequelize } = require("sequelize");
-
-// TODO: any extra db config or init script
-// middleware
 const app = express();
-app.use(
-	cors({
-		origin: "http://localhost:5173",
-		credentials: true,
-	})
-);
-app.use(
-	session({
-		secret: process.env.SESSION_SECRET,
-		resave: false,
-		saveUninitialized: false,
-		cookie: {
-			httpOnly: true,
-			secure: false,
-			maxAge: 10 * 60 * 1000,
-			sameSite: "lax",
-		},
-	})
-);
+const db = require("./models");
+
+const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(passport.initialize());
-app.use(passport.session());
+app.get("/", (req, res) => {
+	res.send("Hello from Heroku!");
+});
 
-//TODO: routing goes here
-const scenarioController = require("./controllers/scenario");
-const authController = require("./controllers/auth");
-const eventController = require("./controllers/eventseries");
-const investmentController = require("./controllers/investment");
-const investmentTypeController = require("./controllers/investmenttype");
-app.use("/scenarios", scenarioController);
-app.use("/auth", authController);
-app.use("/events", eventController);
-app.use("/investments", investmentController);
-app.use("/investmenttypes", investmentTypeController);
-
-// listening
-app.listen(8000, () => {
-	console.log("Server started on port 8000");
+// Sync models and start server
+db.sequelize.sync().then(() => {
+	app.listen(PORT, () => {
+		console.log(`Server running on port ${PORT}`);
+	});
 });
