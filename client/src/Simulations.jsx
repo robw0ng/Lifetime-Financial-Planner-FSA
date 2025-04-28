ce0e424a3a86440c3729f0dce620770a8c0d44ef
Daniel A. Klimatchev <94766312+runformoney47@users.noreply.github.com>
2025-04-28 11:28:08 -0400

    test graph

--- client/src/Simulations.jsx
+++ client/src/Simulations.jsx
@@ -1,7 +1,9 @@
 import { useData } from './DataContext';
+import React, { useRef, useEffect } from 'react';
 import { useSelected } from './SelectedContext';
 import styles from './Simulations.module.css';
-
+import { sampleBarData } from './data/sampleData';
+import * as d3 from 'd3';
 function ScenarioList() {
   const { selectedScenario, setSelectedScenario, deselectScenario } =
     useSelected();
@@ -103,7 +105,69 @@ function Summary(){
   );
 }
 
+export function SuccessGraph({ data = sampleBarData }) {
+  const containerRef = useRef(null);
 
+  useEffect(() => {
+    if (data && containerRef.current) {
+      // Clear any previous content
+      d3.select(containerRef.current).selectAll("*").remove();
+
+      // Set up SVG canvas
+      const svg = d3.select(containerRef.current)
+                    .append("svg")
+                    .attr("width", 500)
+                    .attr("height", 300);
+
+      const margin = { top: 20, right: 30, bottom: 30, left: 40 };
+      const width = 500 - margin.left - margin.right;
+      const height = 300 - margin.top - margin.bottom;
+
+      const g = svg.append("g")
+                   .attr("transform", `translate(${margin.left},${margin.top})`);
+
+      // Create scales
+      const x = d3.scaleBand()
+                  .domain(data.map(d => d.name))
+                  .range([0, width])
+                  .padding(0.1);
+
+      const y = d3.scaleLinear()
+                  .domain([0, d3.max(data, d => d.value)])
+                  .nice()
+                  .range([height, 0]);
+
+      // Append axes
+      g.append("g")
+       .attr("transform", `translate(0,${height})`)
+       .call(d3.axisBottom(x));
+
+      g.append("g")
+       .call(d3.axisLeft(y));
+
+      // Append bars
+      g.selectAll(".bar")
+       .data(data)
+       .enter()
+       .append("rect")
+         .attr("class", "bar")
+         .attr("x", d => x(d.name))
+         .attr("y", d => y(d.value))
+         .attr("width", x.bandwidth())
+         .attr("height", d => height - y(d.value));
+    }
+  }, [data]);
+
+  return (
+    <div className={styles["success-graph"]}>
+      <div className={styles["outer-container"]}>
+        <div className={styles["inner-container"]} ref={containerRef}>
+          {/* D3 will render the bar chart here */}
+        </div>
+      </div>
+    </div>
+  );
+}
 
 function InvestmentsGraph(){
   return (
@@ -128,7 +192,7 @@ export default function Simulations(){
             </div>
             <div className={`${styles['column']} ${styles['col-2']}`}>
               
-              <InvestmentsGraph />
+              <SuccessGraph />
             </div>
         </div>
     );
